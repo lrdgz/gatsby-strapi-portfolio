15943a81ea3aa63bb260009bf3ba926ceca3ce18
Luis Rodriguez <lui.rodriguez@gcnewhorizons.net>
2020-07-01 18:30:06 -0400

    Add PropsTypes Projects and Blog

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,11 +3,16 @@ import PropTypes from "prop-types"
 import Image from "gatsby-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
 const Project = ({ description, title, github, stack, url, image, index }) => {
+
   return <article className="project">
-    <Image fluid={image.childImageSharp.fluid} className="project-img" />
+
+    {image && (
+      <Image fluid={image.childImageSharp.fluid} className="project-img" />
+    )}
+
     <div className="project-info">
       <span className="project-number"> 0{index + 1}.</span>
-      <h3>{title}</h3>
+      <h3>{title || 'Default Title'}</h3>
       <p className="project-desc">{description}</p>
       <div className="project-stack">
         {
@@ -24,6 +29,13 @@ const Project = ({ description, title, github, stack, url, image, index }) => {
   </article>
 }
 
-Project.propTypes = {}
+Project.propTypes = {
+  title: PropTypes.string.isRequired,
+  github: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  stack: PropTypes.arrayOf(PropTypes.object).isRequired
+}
 
 export default Project
